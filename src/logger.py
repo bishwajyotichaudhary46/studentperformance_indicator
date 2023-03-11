ebce8d9dcec01ff61b221a0365d55729ad23256d
Bishwajyoti Chaudhary <bishwajyotichaudhary46@gmail.com>
2023-03-11 22:58:54 +0545

    some upadate in logging and exception

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-log_path = os.path.join(os.get_cw(),'logs', LOG_FILE)
+log_path = os.path.join(os.getcwd(),'logs', LOG_FILE)
 os.makedirs(log_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
@@ -13,3 +13,4 @@ logging.basicConfig(
     format="[%(asctime)s ] %(lineno)s %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
+
